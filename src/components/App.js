2e9bc6f38a028047ba3c23adca63f616d06bc021
bassiebangura <devbassiebangura@gmail.com>
2019-09-02 17:43:06 -0400

    Create Player and Counter Components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "./App.css";
 
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Bassie
+      </span>
+      <Counter />
+    </div>
+  )
+}
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">5</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!!!</p>
-      </header>
-    </div>
+    <Player />
   );
 }
 
