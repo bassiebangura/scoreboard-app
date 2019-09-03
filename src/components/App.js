7d1121091f049f8b65a8388f77faf4ec29d6b75f
bassiebangura <devbassiebangura@gmail.com>
2019-09-03 08:06:01 -0400

    Convert Counter component to class component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,45 @@
 import React from "react";
 import "./App.css";
 
-const Player = () => {
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+};
+
+const Player = props => {
   return (
     <div className="player">
-      <span className="player-name">
-        Bassie
-      </span>
-      <Counter />
+      <span className="player-name">{props.player}</span>
+      <Counter score={props.score} />
     </div>
-  )
-}
+  );
+};
 
-const Counter = () => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">5</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  )
-}
+class Counter extends React.Component {
+
+  render () {
+     return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{props.score}</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  } 
+} 
+ 
 
 
 function App() {
   return (
-    <Player />
+    <div className="scoreboard">
+      <Header />
+      <Player player="Bassie Bangura" score={30} />
+    </div>
   );
 }
 
