ef7dd6bc0e12b3ee4abda73b50809e929f865a73
bassiebangura <devbassiebangura@gmail.com>
2019-09-04 10:52:52 -0400

    Lift state to parent component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Player from "./Player";
+import Stats from "./Stats"
 import "./App.css";
 
 class App extends React.Component {
@@ -8,11 +9,33 @@ class App extends React.Component {
     players: [
       {
         name: "Bassie",
+        score: 10,
         id: 1
       },
-      { name: "Joseph", id: 2 },
-      { name: "Junior", id: 3 }
+      { name: "Joseph", score: 15, id: 2 },
+      { name: "Junior", score: 20, id: 3 },
+      { name: "Bangura", score: 25, id: 4}
     ]
+  };
+
+  handleScoreChange = ( index, delta) => {
+    
+    this.setState( prevState => {
+      // New 'players' array – a copy of the previous `players` state
+      const updatedPlayers = [ ...prevState.players ];
+      // A copy of the player object we're targeting
+      const updatedPlayer = { ...updatedPlayers[index] };
+
+      // Update the target player's score
+      updatedPlayer.score += delta;
+      // Update the 'players' array with the target player's latest score
+      updatedPlayers[index] = updatedPlayer;
+
+      // Update the `players` state without mutating the original state
+      return {
+        players: updatedPlayers
+      };
+    });
   };
 
   handleRemovePlayer = id => {
@@ -27,8 +50,11 @@ class App extends React.Component {
       <div className="scoreboard">
         <Header title="Scoreboard" totalPlayers={this.state.players.length} />
 
-        {this.state.players.map(player => (
+        {this.state.players.map((player, index) => (
           <Player
+          index={index}
+            changeScore={this.handleScoreChange}
+            score={player.score}
             id={player.id}
             name={player.name}
             key={player.id.toString()}
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,20 +1,24 @@
 import React from "react";
-import Counter from "./Counter"
+import Counter from "./Counter";
 const Player = props => {
-    return (
-      <div className="player">
-        <span className="player-name">
-          <button
-            className="remove-player"
-            onClick={() => props.removePlayer(props.id)}
-          >
-            ✖
-          </button>
-          {props.name}
-        </span>
-        <Counter />
-      </div>
-    );
-  };
+  return (
+    <div className="player">
+      <span className="player-name">
+        <button
+          className="remove-player"
+          onClick={() => props.removePlayer(props.id)}
+        >
+          ✖
+        </button>
+        {props.name}
+      </span>
+      <Counter
+        score={props.score}
+        index={props.index}
+        changeScore={props.changeScore}
+      />
+    </div>
+  );
+};
 
 export default Player;
