2682781ab7ac904a5006aa22ac0510ece056e6f1
bassiebangura <devbassiebangura@gmail.com>
2019-09-30 20:58:46 -0400

    Adjust timer.

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,14 +2,41 @@ import React, { Component } from "react";
 
 class Stopwatch extends Component {
   state = {
-    isRunning: false
+    isRunning: false,
+    previousTime: 0,
+    elapsedTime: 0
+  };
+
+  handleStopwatch = () => {
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
+  };
+  componentDidMount() {
+    this.setIntervalID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  tick = () => {
+    console.log("ticking....")
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState(prevState => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+      }));
+    }
   };
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>{this.state.isRunning ? "Stop" : "Start"}</button>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? "Stop" : "Start"}</button>
         <button>Reset</button>
       </div>
     );
